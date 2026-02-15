07abccf32b2a605216152988efac721ad7f98112
24681357aA <2897632899@qq.com>
2026-02-15 21:01:41 +0800

    版本号为0.0.1-alpha-3-1,增加了一些全新的内容。进行少量修改

--- src/tqlslib/TQLSlib.java
+++ src/tqlslib/TQLSlib.java
@@ -3,10 +3,21 @@ package tqlslib;
 import arc.Events;
 import arc.util.Log;
 import mindustry.game.EventType.ClientLoadEvent;
+import mindustry.mod.ClassMap;
 import mindustry.mod.Mod;
 import tqlslib.core.SpaceStationIO;
+import tqlslib.graphics.TQDrawf;
+import tqlslib.type.status.ExtentionStatus;
+import tqlslib.util.TQUtls;
+import tqlslib.worlds.blocks.distribution.CoreUnloader_Import;
+import tqlslib.worlds.blocks.distribution.CoreUnloader_Output;
+import tqlslib.worlds.blocks.distribution.LiquidTransferStation;
+import tqlslib.worlds.blocks.distribution.Liquid_Import;
+import tqlslib.worlds.blocks.distribution.Liquid_Output;
+import tqlslib.worlds.blocks.power.PowerTower;
 
 public class TQLSlib extends Mod{
+    private static final String PREFIX = "tqls-";
 
     public TQLSlib(){
         Log.info("Loaded TQLSlib constructor.");
@@ -18,12 +29,22 @@ public class TQLSlib extends Mod{
         
         DTVars.init();
         
-        Log.info("Loading PW integration content.");
-        PWIntegration pwIntegration = new PWIntegration();
-        pwIntegration.loadContent();
+        Log.info("Registering classes...");
+        ClassMap.classes.put(PREFIX + "PowerTower", PowerTower.class);
+        ClassMap.classes.put(PREFIX + "ExtentionStatus", ExtentionStatus.class);
+        ClassMap.classes.put(PREFIX + "CoreUnloader_Output", CoreUnloader_Output.class);
+        ClassMap.classes.put(PREFIX + "CoreUnloader_Import", CoreUnloader_Import.class);
+        ClassMap.classes.put(PREFIX + "Liquid_Output", Liquid_Output.class);
+        ClassMap.classes.put(PREFIX + "Liquid_Import", Liquid_Import.class);
+        ClassMap.classes.put(PREFIX + "LiquidTransferStation", LiquidTransferStation.class);
+        ClassMap.classes.put(PREFIX + "TQDrawf", TQDrawf.class);
+        ClassMap.classes.put(PREFIX + "TQUtls", TQUtls.class);
+        Log.info("Classes registered successfully with tqls- prefix");
         
-        Log.info("Loading Tranqol integration content.");
-        TranqolIntegration.load();
+        Log.info("Initializing utilities...");
+        TQDrawf.init();
+        TQUtls.init();
+        Log.info("Utilities initialized.");
         
         Log.info("TQLSlib content loaded successfully.");
     }
@@ -39,9 +60,6 @@ public class TQLSlib extends Mod{
             SpaceStationIO.load();
             Log.info("TQLSlib: Space station data loaded.");
         });
-        
-        Log.info("Initializing Tranqol integration.");
-        TranqolIntegration.init();
         
         Log.info("TQLSlib initialized successfully.");
     }
